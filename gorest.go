86d91f2aa89e3aac290cba9af9d85498e178cdce
Janne Vikstedt <janne.vikstedt@gmail.com>
2016-01-22 21:32:51 +0200

    Changed gorilla/mux to julienschmidt/httprouter instead

--- gorest.go
+++ gorest.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
+	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Write([]byte("Hello World\n"))
 }
 
 func main() {
 	fmt.Println("Starting gorest")
 
-	router := mux.NewRouter()
-	router.HandleFunc("/", HomeHandler)
+	router := httprouter.New()
+	router.GET("/", Index)
 
 	http.ListenAndServe(":3000", router)
 }
