0050f7e40687d17d9bfdfd8a181ae75dde05bc43
Janne Vikstedt <janne.vikstedt@gmail.com>
2016-01-22 21:15:21 +0200

    Added gorilla/mux and created basic server

--- gorest.go
+++ gorest.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
 )
 
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World\n"))
+}
+
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Starting gorest")
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", HomeHandler)
+
+	http.ListenAndServe(":3000", router)
 }
